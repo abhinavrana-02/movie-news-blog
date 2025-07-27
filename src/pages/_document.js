5b212a29edc2ee051b01efc890539c6559f67b78
abhinavrana-02 <71113208+abhinavrana-02@users.noreply.github.com>
2025-07-27 18:54:54 +0530

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,14 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        {/* ✅ Google AdSense Script */}
+       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7556593786287737"
+     crossorigin="anonymous"></script>
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
